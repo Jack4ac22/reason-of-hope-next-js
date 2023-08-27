2140b0b7592f676ab769652f0b1fb5ac5296f7fb
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2023-08-27 16:08:50 +0200

    staging

--- components/ui/header.js
+++ components/ui/header.js
@@ -1,13 +1,74 @@
 import UiLogo from "./logo";
-import UiMainNav from "./main-nav";
+import Link from "next/link";
 
 export default function UiHeader() {
   return (
-    <>
-      <header className="container row"> 
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top bg">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="/">
           <UiLogo />
-          <UiMainNav />
-      </header>
-    </>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                البداية
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/objections">
+                اعتراضات
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/creation">
+                {" "}
+                مقالات الخلق
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/logic">
+                {" "}
+                المنطق
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/words">
+                كلمة ورسالة
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/publications">
+                {" "}
+                منشورات
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href="/contact">
+                {" "}
+                اتصل بنا
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
--- components/ui/logo.js
+++ components/ui/logo.js
@@ -1,13 +1,10 @@
-import Link from "next/link";
 import Image from "next/image";
 
 export default function UiLogo() {
   return (
     <>
       <div className="logo col">
-        <Link href="/">
           <Image src="/ROH.png" alt="سبب الرجاء" width={120} height={46} />
-        </Link>
       </div>
     </>
   );

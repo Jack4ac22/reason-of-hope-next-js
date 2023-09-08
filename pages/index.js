09670c1cb9b51d4f74454d055c3a8a677c43d30a
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2023-09-08 17:24:12 +0200

    removing dummy data from index main

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 import styles from "../styles/Home.module.css";
 import HeroMain from "../components/main/hero-main.js";
-
-import FeaturedWords from "../components/word-components/featured-words.js";
+import SampleMain from "../components/main/sample-main.js";
 
 export default function Home() {
-  const dummyWords = [
-    {
-      title: "Lorem Ipsum",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec lacinia sapien.",
-      image: "001.jpg",
-    },
-    {
-      title: "Dolor Sit Amet",
-      content:
-        "Dolor sit amet, consectetur adipiscing elit. Sed nec lacinia sapien.",
-      image: "003-1.jpg",
-    },
-  ];
-
   return (
     <div className={styles.container}>
       <HeroMain />
-
-      <FeaturedWords words={dummyWords} title="Featured Words" />
     </div>
   );
 }

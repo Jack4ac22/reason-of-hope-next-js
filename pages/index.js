d5394a09fc2418a299b8cc341b1e1a3cc64ec7f0
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2023-09-08 17:24:42 +0200

    d

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import styles from "../styles/Home.module.css";
 import HeroMain from "../components/main/hero-main.js";
-import SampleMain from "../components/main/sample-main.js";
 
 export default function Home() {
   return (

aada0f5ca03378e98d0147a9c69dbd6bc61644a4
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2023-12-15 06:04:16 +0200

    Refactor component class names and fix typos

--- components/main/bible-section.js
+++ components/main/bible-section.js
@@ -13,32 +13,32 @@ export default function TheGospelSectionMainPage() {
   };
   return (
     <>
-      <section>
-        <div class="container my-5">
-          <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-            <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-              <h3 class="display-4 fw-bold lh-1 text-body-emphasis">
-                THe Gospel section
+      <section key={"gospel-section"}>
+        <div className="container my-5">
+          <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+            <div className="col-lg-6 p-3 p-lg-5 pt-lg-3">
+              <h3 className="display-4 fw-bold lh-1 text-body-emphasis">
+                The Gospel section
               </h3>
-              <p class="lead">
+              <p className="lead">
                 Gospel section description Gospel section description Gospel
               </p>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+              <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                 <button
                   type="button"
-                  class="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+                  className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
                 >
                   Primary
                 </button>
                 <button
                   type="button"
-                  class="btn btn-outline-secondary btn-lg px-4"
+                  className="btn btn-outline-secondary btn-lg px-4"
                 >
                   Default
                 </button>
               </div>
             </div>
-            <div class="col-lg-4">
+            <div className="col-lg-5" style={{ width: "25rem" }}>
               <YouTube videoId={"98IhK5V53ks"} opts={opts} />
             </div>
           </div>
--- components/main/creation-section.js
+++ components/main/creation-section.js
@@ -5,31 +5,34 @@ export default function CreationSectionMainPage(props) {
   return (
     <>
       <section key={"creation-section"}>
-        <div class="container my-5">
-          <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-            <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-              <h3 class="display-4 fw-bold lh-1 text-body-emphasis">
-Creation Section              </h3>
-              <p class="lead">
+        <div className="container my-5">
+          <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+            <div className="col-lg-6 p-3 p-lg-5 pt-lg-3">
+              <h3 className="display-4 fw-bold lh-1 text-body-emphasis">
+                Creation Section
+              </h3>
+              <p className="lead">
                 Creation Section description Creation Section description
-                Creation
               </p>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+              <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                 <button
                   type="button"
-                  class="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+                  className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
                 >
                   Primary
                 </button>
                 <button
                   type="button"
-                  class="btn btn-outline-secondary btn-lg px-4"
+                  className="btn btn-outline-secondary btn-lg px-4"
                 >
                   Default
                 </button>
               </div>
             </div>
-            <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <div
+              className="col-lg-5"
+              style={{ width: "25rem" }}
+            >
               <AutoplayingCarousel
                 articles={props.articles}
                 id="category_gospel"

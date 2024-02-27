12c750093d8334c72bc84658f7b67c9980c19017
Jack Kazanjyan <Jack4ac22@icloud.com>
2024-02-27 06:41:01 +0100

    Update file paths and fix function names

--- pages/404/index.js
+++ pages/404/index.js
@@ -6,18 +6,24 @@ import { useRouter } from "next/router";
 
 export default function NotFoundPage(props) {
   const articles = props.articles;
-  const randomArticles = randomArticlesFromArray(articles, 8);
+  const randomArticles = props.randomArticles;
   const router = useRouter();
   const { asPath } = router;
 
-  // find the date pattern in the path the date is with this pattern /yyyy/mm/dd
-  const datePattern = asPath.match(/\/\d{4}\/\d{2}\/\d{2}/);
-  // reconstruct the date to be in the format yyyy-mm-dd if the pattern starts with  / skipp it
-  const date = datePattern[0].startsWith("/")
-    ? datePattern[0].replace("/", "")
-    : datePattern[0];
-  const restructeredDate = date.replace(/\//g, "-");
-
+  function getRestructuredDate(path) {
+    // find the date pattern in the path the date is with this pattern /yyyy/mm/dd
+    const datePattern = path.match(/\/\d{4}\/\d{2}\/\d{2}/);
+    if (!datePattern) {
+      return "not found";
+    }
+    // reconstruct the date to be in the format yyyy-mm-dd if the pattern starts with  / skipp it
+    const date = datePattern[0].startsWith("/")
+      ? datePattern[0].replace("/", "")
+      : datePattern[0];
+    const restructeredDate = date.replace(/\//g, "-");
+    return restructeredDate;
+  }
+  const restructeredDate = getRestructuredDate(asPath);
   // find the pattern "objection" followed with a number between 1 and 500 in the path
   const objectionPattern = asPath.match(/objection\d{1,3}/);
 
@@ -52,7 +58,7 @@ export default function NotFoundPage(props) {
         <Link className="btn btn-primary" href="/contact">
           اتصل بنا
         </Link>
-        <h2>المقالات المشابهة</h2>
+        {possibleRelatedArticles.length > 0 && <h2>المقالات المشابهة</h2>}
         {possibleRelatedArticles.map((article) => (
           <ArticleCard
             article={article}
@@ -81,7 +87,7 @@ export async function getStaticProps() {
   return {
     props: {
       articles: articles,
-      randomArticles: randomArticlesFromArray(articles, 3),
+      randomArticles: randomArticlesFromArray(articles, 5),
     },
     revalidate: 30000,
   };

f2764537ec20dd39d4c988c8d9b8b7dd749f4e4f
Jack Kazanjyan <Jack4ac22@icloud.com>
2024-02-29 21:42:34 +0100

    Initialized new project and integrated Vanilla Bootstrap for UI styling.

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col align-self-center">
+            <h1 className="text-danger">Reason Of Hope</h1>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}

9c27f846a14c5375fe9d43810829e3ce7f641ece
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-08-18 10:25:44 +0200

    fix focus for the info button and some minor enhansments for the view

--- src/components/blog-components/cards/article-card/article-card.jsx
+++ src/components/blog-components/cards/article-card/article-card.jsx
@@ -37,8 +37,8 @@ export default function ArticleCard({ article }) {
     
     if (article.slug && featuredArticles.includes(article.slug)) {
       return (
-        <div className='absolute top-2 right-2 flex text-sm text-white bg-mainBrand-500 dark:bg-mainBrand-200
-      border-0 p-1 rounded-md'>
+        <div className='absolute top-2 right-2 flex text-sm text-white bg-mainBrand-500 dark:bg-mainBrand-200 opacity-70 hover:opacity-100
+      border-0 p-1 rounded-md rotate-45 translate-y-6 translate-x-3 duration-200'>
           <FaCross />
           <span className='px-2' >{article.isBook ? "كتاب مُمَيَّز" : " موضوع مُمَيَّز"}</span>
         </div>
@@ -62,7 +62,7 @@ export default function ArticleCard({ article }) {
         <div className='flex-col items-center justify-center h-40 m-2'>
           {/* header including the title and the time */}
           <div>
-            <h2 className='my-2 text-lg text-center font-bold text-mainBrand-600 dark:text-mainBrand-100'>{article.title}</h2>
+            <h2 className='h-12 mbF-2 text-lg  text-center font-bold text-mainBrand-600 dark:text-mainBrand-100'>{article.title.length > 44 ? article.title.slice(0,44) + "..." : article.title}</h2>
             <p className='text-xs text-left text-darkAccent-400 dark:text-white '>
               <span >تاريخ النشر: </span>
               <span>
@@ -75,6 +75,7 @@ export default function ArticleCard({ article }) {
         </div>
         {/* Card Footer container */}
         {/* show information icon */}
+        <span className="sr-only">Close the information card.</span>
         <ShowInformation inlineText={"إضغط  للحصول على المزيد من المعلومات المرتبطة بهذا المنشور."}>
           <ArticleInformation article={article} />
         </ShowInformation>
--- src/components/blog-components/ui/buttons/show-information.jsx
+++ src/components/blog-components/ui/buttons/show-information.jsx
@@ -1,18 +1,21 @@
 'use client'
-import { FaCaretUp } from "react-icons/fa";
 import { useState } from "react";
-import { FaRegWindowClose } from "react-icons/fa";
+import { FaInfoCircle, FaRegWindowClose } from "react-icons/fa";
+
 export default function ShowInformation({ inlineText, children }) {
   const [showInformation, setShowInformation] = useState(false)
   return (
     <>
       {/* section container */}
-      <div className="group animate duration-200">
+      <div className="group animate duration-200 focuse:outline-2">
+
         {/* show the full details of the article */}
-        <button className="relative flex items-center text-mainBrand-500 cursor-pointer hover:text-mainBrand-600 dark:text-mainBrand-300 dark:hover:bg-mainBrand-200">
-          {/* clickable icon to show more */}
+        <button className="relative flex items-center text-mainBrand-500 cursor-pointer hover:text-mainBrand-600 dark:text-mainBrand-300 dark:hover:bg-mainBrand-200
+        focus:text-xl focus:drop-shadow-2xl focus:outline-2"
+          onClick={() => { setShowInformation(true) }}>
+          <FaInfoCircle className="absolute hover:translate-y-0.5 hover:animate-ping" />
           <span className="absolute right-0 sr-only">إظهار المعلومات</span>
-          <FaCaretUp className="absolute hover:translate-y-0.5 hover:animate-ping" onClick={() => { setShowInformation(true) }} />
+
           <div className="relative">
             <div className="absolute bottom-0 hidden group-hover:inline-block w-64 px-4 py-3 mb-1 -ml-32 text-mainBrand-600 dark:text-mainBrand-200
             bg-mainBrand-100 dark:bg-mainBrand-800 
@@ -22,6 +25,8 @@ export default function ShowInformation({ inlineText, children }) {
             </div>
           </div>
         </button>
+
+{/* article informations section */}
         <div className={`w-72 h-96 border rounded-md absolute top-0 left-0 p-4 flex-col items-center bg-lightShade-500 dark:bg-lightShade-800 ${showInformation ? 'opacity-90' : 'hidden'}`} onClick={() => { setShowInformation(false) }}>
           <button className="m-2 text-mainBrand-500" onClick={() => { setShowInformation(false) }
           }>
@@ -30,6 +35,7 @@ export default function ShowInformation({ inlineText, children }) {
           </button>
           {children}
         </div>
+
       </div>
     </>
   )

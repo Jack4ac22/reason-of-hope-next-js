c594c100b2c40379c05240b61ae7ed09846dcfb0
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-09-03 22:59:00 +0200

    add words metadata

--- src/app/(blog)/(collection)/words/page.jsx
+++ src/app/(blog)/(collection)/words/page.jsx
@@ -1,3 +1,5 @@
+import WordsPageMetadata from "@/assets/blog/metadata/words";
+export const metadata = WordsPageMetadata
 export default function WordsPage() {
   return (
     <div>words page (WIP)</div>

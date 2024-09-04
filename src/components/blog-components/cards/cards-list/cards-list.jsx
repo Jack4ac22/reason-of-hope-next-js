c1c7606f4d3d5a32e5f2875191cbb3655c183d2f
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-09-04 10:57:40 +0200

    add suspense to wrap the article list

--- src/components/blog-components/cards/cards-list/cards-list.jsx
+++ src/components/blog-components/cards/cards-list/cards-list.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { Suspense } from 'react'
 import { useSearchParams } from "next/navigation";
 import ArticleCard from "../article-card/article-card";
 export default function CardList({ articles }) {
@@ -36,49 +37,50 @@ export default function CardList({ articles }) {
     <section className="mt-6 uni-text-color">
       <h2 className="text-2xl">مقالات هذه الفئة: </h2>
       <p className="sr-only">Articles</p>
-      <div className="border">
-
-        {/* order articles */}
-        <div className="flex justify-center items-center">
-          <label htmlFor="order" className="mx-2">ترتيب حسب:</label>
-          <select id="order" name="order" value={order} onChange={(e) => handleOrderChange(e.target.value)} className="mx-2">
-            <option value="latest" defaultValue={order === "latest"}>الأحدث</option>
-            <option value="oldest" defaultValue={order === "oldest"}>الأقدم</option>
-            <option value="title" defaultValue={order === "title"}>العنوان</option>
-          </select>
-        </div>
-        {/* number of articles per page */}
-        <div className="flex justify-center items-center">
-          <label htmlFor="per-page" className="mx-2">عدد المقالات في الصفحة:</label>
-          <select id="per-page" name="per-page" value={articles_per_page} onChange={(e) => handlePerPageChange(e.target.value)} className="mx-2">
-            {per_page_options.map((option, index) => (
-              <option key={index} value={option} defaultValue={articles_per_page === option}>{option}</option>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="border">
+          {/* order articles */}
+          <div className="flex justify-center items-center">
+            <label htmlFor="order" className="mx-2">ترتيب حسب:</label>
+            <select id="order" name="order" value={order} onChange={(e) => handleOrderChange(e.target.value)} className="mx-2">
+              <option value="latest" defaultValue={order === "latest"}>الأحدث</option>
+              <option value="oldest" defaultValue={order === "oldest"}>الأقدم</option>
+              <option value="title" defaultValue={order === "title"}>العنوان</option>
+            </select>
+          </div>
+          {/* number of articles per page */}
+          <div className="flex justify-center items-center">
+            <label htmlFor="per-page" className="mx-2">عدد المقالات في الصفحة:</label>
+            <select id="per-page" name="per-page" value={articles_per_page} onChange={(e) => handlePerPageChange(e.target.value)} className="mx-2">
+              {per_page_options.map((option, index) => (
+                <option key={index} value={option} defaultValue={articles_per_page === option}>{option}</option>
+              ))}
+            </select>
+          </div>
+          {/* articles cards */}
+          <div className="flex flex-wrap justify-around items-center">
+            {current_articles.map((article, index) => (
+              <ArticleCard key={`${index}_${article.slug}`} article={article} />
             ))}
-          </select>
-        </div>
-        {/* articles cards */}
-        <div className="flex flex-wrap justify-around items-center">
-          {current_articles.map((article, index) => (
-            <ArticleCard key={`${index}_${article.slug}`} article={article} />
-          ))}
-        </div>
-        {/* pagination */}
-        {total_pages !== 1 && (<div className="flex justify-center items-center">
-          <ul className="flex justify-center items-center">
-            <li className="mx-2">
-              <button onClick={() => handlePageChange(page - 1)} className="text-blue-500 hover:underline disabled:hidden" aria-label="Previous Page" disabled={page == 1}>السابق</button>
-            </li>
-            {Array.from({ length: total_pages }, (_, i) => (
-              <li key={i} className="mx-2">
-                <button onClick={() => handlePageChange(i + 1)} className={`text-blue-500 hover:underline ${page==i+1 && ' text-mainBrand-500'}`} aria-label={`Page ${i + 1}`}>{i + 1}</button>
+          </div>
+          {/* pagination */}
+          {total_pages !== 1 && (<div className="flex justify-center items-center">
+            <ul className="flex justify-center items-center">
+              <li className="mx-2">
+                <button onClick={() => handlePageChange(page - 1)} className="text-blue-500 hover:underline disabled:hidden" aria-label="Previous Page" disabled={page == 1}>السابق</button>
               </li>
-            ))}
-            <li className="mx-2">
-              <button onClick={() => handlePageChange(page + 1)} className="text-blue-500 hover:underline disabled:hidden" aria-label="Next Page" disabled={page == total_pages}>التالي</button>
-            </li>
-          </ul>
-        </div>)}
-      </div>
+              {Array.from({ length: total_pages }, (_, i) => (
+                <li key={i} className="mx-2">
+                  <button onClick={() => handlePageChange(i + 1)} className={`text-blue-500 hover:underline ${page == i + 1 && ' text-mainBrand-500'}`} aria-label={`Page ${i + 1}`}>{i + 1}</button>
+                </li>
+              ))}
+              <li className="mx-2">
+                <button onClick={() => handlePageChange(page + 1)} className="text-blue-500 hover:underline disabled:hidden" aria-label="Next Page" disabled={page == total_pages}>التالي</button>
+              </li>
+            </ul>
+          </div>)}
+        </div>
+      </Suspense>
     </section>
   );
 }

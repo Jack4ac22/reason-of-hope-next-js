0886401d30c8299cc3c52c93119303d49e6f7bc0
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-09-23 10:32:40 +0200

    updating the footers and the gospel page

--- src/components/blog-components/article-page/article-footer.jsx
+++ src/components/blog-components/article-page/article-footer.jsx
@@ -1,5 +1,6 @@
 import RelatedArticles from '@/components/blog-components/article-page/article-footer-components/related-articles';
 export default function ArticleFooter({ article }) {
+  if (article.directory === "static-pages") return null
   return (
     <>
       <RelatedArticles article={article} />

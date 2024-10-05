f6bf0382175e79fd837b664ffd50bee55707d4b6
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-10-05 23:17:48 +0200

    Add search page component with search functionality

---
+++ src/app/(blog)/(articles)/search/page.jsx
@@ -0,0 +1,13 @@
+export default function SearchPage() {
+  return (
+    <div>Page under construction</div>
+
+    // search componenet which will read searchParam named seach, it takes all articles and filter them based on the searchParam.
+    //  if no searchParam is provided, it will show a message to enter a searchParam.
+    // on typing in the search bar, it will show the articles that match the searchParam with a delay of 0.5s.
+    // if no articles match the searchParam, it will show a message that no articles match the searchParam
+    // if there are articles that match the searchParam, it will show the articles in a cardlist.
+    // searchcomponent will be on top of the cardlist.
+    // create a search bar to be added to the layout. it will have a magna glass icon that will display an input text on click then on enter or search click it will redirect to seach page with searchparams that has search key and value of input.
+  );
+}

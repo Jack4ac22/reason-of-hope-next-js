49bd9e2110f5ff320b235dcbe4baa131959fe9c4
Jack Kazanjyan <jack4ac22@yahoo.com>
2025-07-11 10:34:22 +0200

    add pdf viewr component

--- src/components/blog-components/article-page/article-footer.jsx
+++ src/components/blog-components/article-page/article-footer.jsx
@@ -1,11 +1,13 @@
 import RelatedArticles from '@/components/blog-components/article-page/article-footer-components/related-articles';
 import FallaciesList from "@/components/blog-components/article-page/article-footer-components/fallacies-list"
 import CatTagFooter from '@/components/blog-components/article-page/article-footer-components/categories-tags-footer';
+import PdfViewer from '@/components/uneversal-items/pdf-viewer';
 export default function ArticleFooter({ article }) {
   if (article?.directory === "static-pages") return null
   return (
     <>
       <FallaciesList article={article} />
+      {/* <PdfViewer url={article?.pdfUrl} /> */}
       <RelatedArticles article={article} />
       <CatTagFooter article={article} />
     </>

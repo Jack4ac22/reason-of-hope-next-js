d1b2c303ea7ae61d376bd77b9c4cd17bab4621af
Jack Kazanjyan <jack4ac22@yahoo.com>
2025-07-11 10:46:39 +0200

    implement resources function and adjust link

--- src/components/blog-components/article-page/article-footer.jsx
+++ src/components/blog-components/article-page/article-footer.jsx
@@ -2,12 +2,16 @@ import RelatedArticles from '@/components/blog-components/article-page/article-f
 import FallaciesList from "@/components/blog-components/article-page/article-footer-components/fallacies-list"
 import CatTagFooter from '@/components/blog-components/article-page/article-footer-components/categories-tags-footer';
 import PdfViewer from '@/components/uneversal-items/pdf-viewer';
+import { getResourcesLinksFromArticle } from '@/utils/blog/general-functions';
 export default function ArticleFooter({ article }) {
   if (article?.directory === "static-pages") return null
+
+  const pdfLink = getResourcesLinksFromArticle(article)["pdf"];
+  if (article.directory === "static-pages") return null
   return (
     <>
       <FallaciesList article={article} />
-      {/* <PdfViewer url={article?.pdfUrl} /> */}
+      {pdfLink && <PdfViewer url={pdfLink} />}
       <RelatedArticles article={article} />
       <CatTagFooter article={article} />
     </>
